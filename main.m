67e5a68707b4f2a9956629aaf479828536666432
kmayo3 <katlyn.mayo@ymail.com>
2015-11-10 13:02:05 -0700

    Comments

--- main.m
+++ main.m
@@ -1,36 +1,52 @@
-#! octave -qf
+%% clear everything before it is run
 clc
+clear all
+clf
+
+ %% image package
 pkg load image;
+
+%% set file name to the train.txt file
 file_name = 'train.txt';
 
+%% open and read the train file 
 fileID = fopen(file_name, 'r');
-faces = textread(file_name, "%s");
-fclose(fileID);
-r = rows(faces);
-person(:,:)="";
 
+%% reads in the names 
+faces = textread(file_name, "%s");
+
+%% close the file 
+fclose(fileID);
+
+%% set r to the rows of the name
+r = rows(faces);
+
+%% reads the whitespace
+person(:,:)= "";
+
+%% for loop which starts at first image of row
 for i = 1:r;
 
-%%X = 
-%%imagesc(X);
-%%[m n] =size(faces);
-%%img = file_name;
+%% Append images
 T = imread(strcat('train./',char(faces(i)))); 
 
-%reshape into matrix
+%% reshape into matrix
 B = imagesc(reshape(T, 250,250));
+
+%% set colormap to gray 
 colormap('gray');
 
-%append to training
-
-%%person(:,i) = T;
+%% ends the for loop
 endfor
+
+
 %define average
 Average = mean(B, 2);
 imshow(Average);
 
 %%mean = (average/ n-1);
- %average = mean(A, 'r');
+%%average = mean(A, 'r');
+ 
 %compute covariance
 %cov = x*y'
 %%N = cov (x) = 1/N-1 * SUM_i (x(i) - mean(x)) * (y(i) - mean(y));
@@ -41,7 +57,7 @@ Xm = bsxfun(@minus,double (B), mu);
 C = cov(Xm);
 [V,D] = eig(C);
 
-% sort eigenvectors desc
+% sort eigenvectors descend
 [D, i] = sort(diag(D), 'descend');
 V = V(:,i);
 
